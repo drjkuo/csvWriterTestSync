4eab4ee3d8d7fa8288423ea13f8f91abf141689b
drjkuo <drjkuo@gmail.com>
2017-04-26 16:06:33 -0700

    Bug
    _stream_readable.js:503
      dest.on('unpipe', onunpipe);
           ^
    
    TypeError: dest.on is not a function
        at CsvWriteStream.Readable.pipe (_stream_readable.js:503:8)
        at fs.open (/Users/chiayuank/Github/nodeProjects/cvsWriterTestSync/index.js:20:10)
        at FSReqWrap.oncomplete (fs.js:114:15)

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 // writer.end()
 
 
-fs.openSync(`out.csv`, 'w+', (err, fd) => {
+fs.open("out.csv", 'w+', (err, fd) => {
   if (err) {
     if (err.code === 'EEXIST') {
       console.error('myfile already exists');
@@ -20,4 +20,7 @@ fs.openSync(`out.csv`, 'w+', (err, fd) => {
   writer.pipe(fd);
   writer.write({hello: "world", foo: "bar", baz: "taco"})
   writer.end();
+
+  // mkdirp.sync(path.join(base_path, 'content', base));
+  // fs.writeFileSync(path.join(base_path, 'content', base, filename), JSON.stringify(sort(data), null, 2)+'\n');
 });
